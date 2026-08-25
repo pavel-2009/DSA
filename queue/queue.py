1e785bdae43778a00eec7b2c186778719cb74b7f
Павел <ms.radionova@bk.ru>
2026-08-25 14:13:08 +0500

    need to fix

--- queue/queue.py
+++ queue/queue.py
@@ -26,3 +26,37 @@ class Queue:
 
     def empty(self):
         return not self.in_stack and not self.out_stack
+
+
+def process_commands(commands: list[str], queue: Queue):
+    commands_available = {
+        "push": queue.push,
+        "pop": queue.pop
+    }
+
+    for command in commands:
+        com_params = command.split(' ')
+        if len(com_params) == 1:
+            com = com_params[0]
+            commands_available[com]()
+        elif len(com_params) == 2:
+            com, val = com_params
+            commands_available[com](val)
+        else:
+            print("Unknown command")
+
+    print(queue.in_stack)
+    print(queue.out_stack)
+
+if __name__ == '__main__':
+    queue = Queue()
+
+    commands = [
+        "push 5",
+        "push 10",
+        "pop",
+        "push 20",
+        "pop",
+    ]
+
+    process_commands(commands, queue)
